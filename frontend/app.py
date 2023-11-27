82e23090f770b5d6f9d31facf729a78911da16ec
Jeff Lin <jjl9895@nyu.edu>
2023-11-27 17:28:20 -0500

    Created a Login Page
    Created a login page

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+
 
 app = Flask(__name__)
 
 # Database configuration
 db_config = {
-    'host': 'localhost',             # or your database host
-    'user': 'Jeff',         # your database username
-    'password': '[_ODOim51K7VM9fi',     # your database password
-    'database': 'AirportProject' # your database name
+    'host': 'localhost',            
+    'user': 'Jeff',         
+    'password': '[_ODOim51K7VM9fi',    
+    'database': 'AirportProject'
 }
 
 # Establishing a database connection
@@ -16,20 +17,50 @@ def get_db_connection():
     connection = mysql.connector.connect(**db_config)
     return connection
 
-@app.route('/')
+# Login page
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        if check_credentials(email, password):
+            return redirect(url_for('home'))  # Redirect to home page if login is successful
+        else:
+            error = 'Invalid credentials'
+
+    return render_template('login.html', error=error)
+
+def check_credentials(email, password):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    if user and password:
+        return True
+    return False
+
+@app.route('/home')
+def home():
+    return "Welcome to the home page"
+
+
+@app.route('/index')
 def index():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Example query - modify according to your database structure
     cursor.execute('SELECT * FROM Airline')
     data = cursor.fetchall()
 
     cursor.close()
     conn.close()
 
-    # You can format the data as you prefer to send it to your frontend
-    return str(data)  # For now, just returning raw data
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
